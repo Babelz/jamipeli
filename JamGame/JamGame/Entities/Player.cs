d2e03beac9e0c46684a9ae038f018be9fb063c4c
siquel <squual@kahvipaussi.net>
2013-11-23 02:11:01 +0200

    Lisätty farseer physics engine ja playerille fysiikat.

--- JamGame/JamGame/Entities/Player.cs
+++ JamGame/JamGame/Entities/Player.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Joints;
+using FarseerPhysics.Factories;
 using JamGame.Components;
 using JamGame.GameObjects;
 using JamGame.Input;
@@ -16,15 +20,45 @@ namespace JamGame.Entities
         private MotionEngine motionEngine;
         private InputControlSetup defaultSetup;
         private InputController controller;
-        private float speed = 5f;
+        private float speed = 15f;
 
-        public Player()
+        private Body body;
+
+        public Player(World world)
         {
             motionEngine = new MotionEngine(this);
             defaultSetup = new InputControlSetup();
             controller = new InputController(Game.Instance.InputManager);
             controller.ChangeSetup(defaultSetup);
+            body = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(100), ConvertUnits.ToSimUnits(100), 1.0f);
+            body.Mass = 0.1f;
+            body.Friction = 1f;
+            body.BodyType = BodyType.Dynamic;
+            body.Restitution = 0f;
+            body.LinearDamping = 5f;
+            Position = Vector2.Zero;
+            
+            
+            
             Initialize();
+        }
+
+        public override Vector2 Position
+        {
+            get
+            {
+                return new Vector2(
+                    ConvertUnits.ToDisplayUnits(body.Position.X),
+                    ConvertUnits.ToDisplayUnits(body.Position.Y)
+                    );
+            }
+            set
+            {
+                body.Position = new Vector2(
+                    ConvertUnits.ToSimUnits(value.X),
+                    ConvertUnits.ToSimUnits(value.Y)
+                    );
+            }
         }
 
         public void Initialize()
@@ -36,19 +70,16 @@ namespace JamGame.Entities
         private void InitKeyMaps()
         {
             var keymapper = defaultSetup.Mapper.GetInputBindProvider<KeyInputBindProvider>();
-            keymapper.Map(new KeyTrigger("move left", Keys.A),(triggered, args) =>  motionEngine.GoalVelocityX = -VelocityFunc(args, speed));
-            keymapper.Map(new KeyTrigger("move right", Keys.D), (triggered, args) => motionEngine.GoalVelocityX = VelocityFunc(args, speed));
-            keymapper.Map(new KeyTrigger("move up", Keys.W), (triggered, args) => motionEngine.GoalVelocityY = -VelocityFunc(args, speed));
-            keymapper.Map(new KeyTrigger("move down", Keys.S), (triggered, args) => motionEngine.GoalVelocityY = VelocityFunc(args, speed));
-        }
-
-        private float VelocityFunc(InputEventArgs args, float src)
-        {
-            if (args.State == InputState.Released)
-            {
-                return 0;
-            }
-            return src;
+            keymapper.Map(new KeyTrigger("move left", Keys.A),
+                (triggered, args) =>
+                    body.ApplyForce(new Vector2(-speed, 0)));
+            keymapper.Map(new KeyTrigger("move right", Keys.D),
+                (triggered, args) => body.ApplyForce(new Vector2(speed, 0)));
+            keymapper.Map(new KeyTrigger("move up", Keys.W),
+                (triggered, args) => body.ApplyForce(new Vector2(0, -speed)));
+            keymapper.Map(new KeyTrigger("move down", Keys.S),
+                (triggered, args) => body.ApplyForce(new Vector2(0, speed)));
+            
         }
 
         public override void Update(GameTime gameTime)
@@ -59,17 +90,17 @@ namespace JamGame.Entities
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            base.Draw(spriteBatch);
-            spriteBatch.Draw(Game.Instance.Temp, new Rectangle((int) Position.X, (int) Position.Y, 100,100), Color.Black );
+            spriteBatch.Draw(Game.Instance.Temp, new Rectangle((int) Position.X,
+                (int) Position.Y, 100, 100), Color.Black);
         }
 
         private void InitPadMaps()
         {
             var padmapper = defaultSetup.Mapper.GetInputBindProvider<PadInputBindProvider>();
-            padmapper.Map(new ButtonTrigger("move left", Buttons.LeftThumbstickLeft), (triggered, args) => motionEngine.GoalVelocityX = -VelocityFunc(args, speed));
-            padmapper.Map(new ButtonTrigger("move right", Buttons.LeftThumbstickRight), (triggered, args) => motionEngine.GoalVelocityX = VelocityFunc(args, speed));
-            padmapper.Map(new ButtonTrigger("move up", Buttons.LeftThumbstickUp), (triggered, args) => motionEngine.GoalVelocityY = -VelocityFunc(args, speed));
-            padmapper.Map(new ButtonTrigger("move down", Buttons.LeftThumbstickDown), (triggered, args) => motionEngine.GoalVelocityY = VelocityFunc(args, speed));
+            padmapper.Map(new ButtonTrigger("move left", Buttons.LeftThumbstickLeft), (triggered, args) => body.ApplyForce(new Vector2(-speed, 0)));
+            padmapper.Map(new ButtonTrigger("move right", Buttons.LeftThumbstickRight), (triggered, args) => body.ApplyForce(new Vector2(speed, 0)));
+            padmapper.Map(new ButtonTrigger("move up", Buttons.LeftThumbstickUp), (triggered, args) => body.ApplyForce(new Vector2(0, -speed)));
+            padmapper.Map(new ButtonTrigger("move down", Buttons.LeftThumbstickDown), (triggered, args) => body.ApplyForce(new Vector2(0, speed)));
 
         }
     }
--- JamGame/JamGame/Gamestate/GameplayState.cs
+++ JamGame/JamGame/Gamestate/GameplayState.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics;
 using JamGame.Entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,21 +14,29 @@ namespace JamGame.Gamestate
     {
         private Player player;
 
+        private World world;
+        private Wall wall;
         public GameplayState()
         {
-            player = new Player();
+            world = new World(new Vector2(0f, 0f));
+            player = new Player(world);
+            wall = new Wall(world, new Vector2(0, 300), 1000, 100);
+
         }
 
         public override void Update(GameTime gameTime)
         {
-            player.Update(gameTime);   
+            world.Step((float) (gameTime.ElapsedGameTime.TotalMilliseconds * .001));
+           // player.Update(gameTime);   
         }
+
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             spriteBatch.GraphicsDevice.Clear(Color.Red);
             player.Draw(spriteBatch);
+            wall.Draw(spriteBatch);
             spriteBatch.End();
         }
     }
